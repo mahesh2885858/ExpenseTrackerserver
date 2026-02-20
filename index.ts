338cbb12dfc9bbbcd2cbc63f8e1a80020a2f956e
mahesh <mahesh2885858@gmail.com>
2026-02-20 21:32:52 +0530

    chore: add swagger

--- index.ts
+++ index.ts
@@ -3,18 +3,54 @@ const fastify = Fastify({
   logger: true,
 });
 
-// Declare a route
-fastify.get("/", async function handler(request, reply) {
-  return { hello: "world" };
+await fastify.register(import("@fastify/swagger"));
+
+await fastify.register(import("@fastify/swagger-ui"), {
+  routePrefix: "/documentation",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
 });
 
-fastify.get("/user", async function handler(request, reply) {
-  return { hello: "user", req: request.url };
+fastify.route({
+  method: "get",
+  url: "/",
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          name: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+  handler: function (req, res) {
+    return { name: "online" };
+  },
 });
 
 // Run the server!
 try {
   await fastify.listen({ port: 3000 });
+  await fastify.ready();
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
