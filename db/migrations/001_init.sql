e4b33d34c841ffa13401cb2b2a65be9421d75fe5
maheshbabu <mahesh2885858@gmail.com>
2026-03-02 16:07:25 +0530

    implement more robust migration stystem

--- db/migrations/001_init.sql
+++ db/migrations/001_init.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
-)
+);
--- db/runMigrations.ts
+++ db/runMigrations.ts
@@ -2,26 +2,76 @@ import { DatabaseSync } from "node:sqlite";
 import path from "node:path";
 import { readdir, readFile } from "node:fs/promises";
 import AppError from "../utils/error.ts";
+import { createHash } from "node:crypto";
+
+const createMigrationIfNotExistIndb = async (db: DatabaseSync) => {
+  const createDBStmt = db.prepare(`
+    CREATE TABLE IF NOT EXISTS migrations(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    checksum TEXT NOT NULL,
+    applied_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+    `);
+
+  createDBStmt.run();
+};
+
+const runMigrationFiles = async (
+  filesInTheDir: string[],
+  directoryWhichHasMigrations: string,
+  db: DatabaseSync,
+) => {
+  for (const file of filesInTheDir) {
+    try {
+      const filePath = path.join(directoryWhichHasMigrations, file);
+      const sql = await readFile(filePath, "utf-8");
+      if (sql.trim().length === 0)
+        throw new Error("Invalid sql from file: " + filePath);
+
+      const normalizedSql = sql.replace(/\r\n/g, "\n");
+      const hash = createHash("sha256");
+      hash.update(normalizedSql, "utf-8");
+      const checksum = hash.digest("hex");
+      db.exec("BEGIN;");
+      db.exec(sql);
+      const prepareStmt = db.prepare(`
+            INSERT INTO migrations (name,checksum) VALUES (?,?);
+            `);
+      prepareStmt.run(file, checksum);
+      db.exec("COMMIT;");
+    } catch (err) {
+      if (db.isTransaction) {
+        db.exec("ROLLBACK;");
+      }
+      throw err;
+    }
+  }
+};
 
 export async function runMigrations(db: DatabaseSync) {
   try {
+    await createMigrationIfNotExistIndb(db);
+
     // get the directory which contains migration files
     const directoryWhichHasMigrations = path.join(
       import.meta.dirname,
       "migrations",
     );
-    console.log({
-      directoryWhichHasMigrations,
-    });
 
     const filesInTheDir = await readdir(directoryWhichHasMigrations);
-    console.log({
-      filesInTheDir,
-    });
+
     if (filesInTheDir.length === 0) {
       throw new Error(
         "No migration files found in the folder: " +
           directoryWhichHasMigrations,
+      );
+    }
+
+    const uniqueFiles = new Set(filesInTheDir);
+    if (filesInTheDir.length !== uniqueFiles.size) {
+      throw new Error(
+        "Same name was found. Migration files should have unique names",
       );
     }
 
@@ -35,28 +85,77 @@ export async function runMigrations(db: DatabaseSync) {
     // sort the files (old sql files first and new sql files should come last)
     filesInTheDir.sort();
 
-    console.log({ sortedFiles: filesInTheDir });
+    // get the migration names from migrations table
+    const appliedMigrationsStmt = db.prepare(`
+      SELECT  *  FROM migrations ORDER BY name;
+      `);
 
-    const statementsToExec = [];
-    for (const file of filesInTheDir) {
-      const filePath = path.join(directoryWhichHasMigrations, file);
-      const sql = await readFile(filePath, "utf-8");
-      if (sql.trim().length === 0)
-        throw new Error("Invalid sql from file: " + filePath);
-      statementsToExec.push(sql);
-    }
+    const appliedMigrations = appliedMigrationsStmt.all();
 
-    console.log({ statementsToExec });
-    if (statementsToExec.length === 0) {
-      console.log("Nothing to run!!");
+    if (appliedMigrations.length === 0) {
+      await runMigrationFiles(filesInTheDir, directoryWhichHasMigrations, db);
+      console.log("Migration completed!!!");
     } else {
-      for (const sql of statementsToExec) {
-        db.exec(sql);
+      const appliedSet = new Set(appliedMigrations.map((i) => i.name));
+
+      // Make sure no migration file is deleted from folder which has already ran. and checksum is verified
+      for (const migration of appliedMigrations) {
+        const isDeleted =
+          filesInTheDir.findIndex((file) => file === migration.name) === -1;
+
+        if (isDeleted)
+          throw new Error(
+            `Applied migration ${migration.name} is missing from local migration directory. Migration history must be immutable. Please restore the file or reset the database.`,
+          );
+
+        const sql = await readFile(
+          path.join(directoryWhichHasMigrations, String(migration.name)),
+          "utf-8",
+        );
+        const normalizedSql = sql.replace(/\r\n/g, "\n");
+
+        const hash = createHash("sha256");
+
+        hash.update(normalizedSql, "utf-8");
+        const checksumLocal = hash.digest("hex");
+        const checkSumDb = migration.checksum;
+        const isCheckSumVerified = checkSumDb === checksumLocal;
+        if (!isCheckSumVerified) {
+          throw new Error(
+            "Migration checksum mismatch detected for the file " +
+              migration.name +
+              ". Please revert the file to it's original state or reset the database.",
+          );
+        }
       }
+
+      // find the first element which is not run yet
+      const t = filesInTheDir.findIndex((file) => !appliedSet.has(file));
+
+      if (t === -1) {
+        console.log("Nothing to run!!!!!");
+        return;
+      }
+
+      const remainingMigrationsToRun = filesInTheDir.slice(t, undefined);
+
+      // make sure the remaining migrations are never ran,
+      remainingMigrationsToRun.forEach((m) => {
+        if (appliedSet.has(m)) {
+          throw new Error(
+            `The migrations file ${m}, should not have ran. May be there is a gap in the migrations order please manually check this to ensure your previous migration ran properly.`,
+          );
+        }
+      });
+
+      await runMigrationFiles(
+        remainingMigrationsToRun,
+        directoryWhichHasMigrations,
+        db,
+      );
+
+      console.log("Migration completed!!!");
     }
-    // const sql = await readFile(migrationFilePath, "utf-8");
-    // db.exec(sql);
-    console.log("Migration completed!!!");
   } catch (e: any) {
     throw new AppError(e.message, "ERR_MIGRATION", 500);
   }
